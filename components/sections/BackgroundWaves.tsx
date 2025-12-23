ebec3fe82bbffda61af6922181831364ea4e244b
OddFutureDaniel <danielmatt@hotmail.co.uk>
2025-12-23 17:15:40 +0000

    fixed button component, removed dead pages

--- components/sections/BackgroundWaves.tsx
+++
@@ -1,9 +0,0 @@
-import AnimatedSvg from "./AnimatedWaveBackground";
-
-export default function BackgroundWaves() {
-  return (
-    <div className="pointer-events-none absolute inset-x-0 top-0 h-[180vh] md:h-[220vh] z-10">
-      <AnimatedSvg />
-    </div>
-  );
-}
